707d3c0265043cbf26d10cc63772cd4ba20dea28
Rageshwari <ragu.bodke@gmail.com>
2020-03-20 12:58:50 +0530

    Calculating wages till a given condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,17 +2,22 @@
 
 echo "Welcome to employee Wage problem"
 
-isPresent=1
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-numWorkingDays=20
+#Constants
+IS_PRESENT=1
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+MAX_WORKING_DAYS=20
+#Variables
 totolSalary=0
+totalEmpHrs=0
+totalWorkingDays=0
+salary=0
 
 employeeCheck=$((RANDOM%3))
 
 #To check whether the employee is present or absent 
-if [ $isPresent -eq $employeeCheck ]
+if [ $IS_PRESENT -eq $employeeCheck ]
 then
 	echo "Employee is Present"
 else
@@ -20,10 +25,10 @@ else
 fi
 
 #To calculate daily employee wage
-if [ $isPresent -eq $employeeCheck ]
+if [ $IS_PRESENT -eq $employeeCheck ]
 then 
 	empHrs=8
-	salary=$(($empHrs*$empRatePerHr))
+	salary=$(($empHrs*$EMP_RATE_PER_HR))
 else
 	salary=0
 fi
@@ -33,17 +38,38 @@ fi
 #To add part time employee and calculate it's wage using case statement
 
 #To calculate the wage for month
-for (( day=1; day<=$numWorkingDays; day++ ))
+for (( day=1; day<=$MAX_WORKING_DAYS; day++ ))
 do
 	employeeCheck=$((RANDOM%3))
 		case $employeeCheck in
-			$isFullTime)
+			$IS_FULL_TIME)
 				empHrs=8 ;;
-			$isPartTime)
+			$IS_PART_TIME)
 				empHrs=4 ;;
 			*)
 				empHrs=0 ;;
 		esac
-		salary=$(($empHrs*$empRatePerHr))
+		salary=$(($empHrs*$EMP_RATE_PER_HR))
 		totalSalary=$(($totalSalary+$salary))
 done
+
+#Calculate wages till a condition of total working hours or days is reached for a month
+# VARIABLES
+
+while [[ $totalEmpHrs -lt $MAX_WORKING_DAYS && $totalWorkingDays -lt $MAX_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	employeeCheck=$((RANDOM%3))
+	case $employeeCheck in
+		$IS_FULL_TIME)
+			empHrs=8 ;;
+		$IS_PART_TIME)
+			empHrs=4 ;;
+		*)
+			empHrs=0 ;;
+	esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
